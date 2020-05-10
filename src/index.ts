b22acf24c8a29a6ad7133bb8abbd5a814ddeff5a
Marko Harjula <marko.harjula@gmail.com>
2020-05-10 17:08:05 +0000

    base class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,39 @@
-import * as EventEmitter from 'events';
+import {EventEmitter} from 'events';
 
-export class BubbleEvent extends EventEmitter {
+interface ILocation {
+	id: string;
+	location: {
+		lat: number;
+		lon: number;
+	};
+}
 
+interface IDoubleBubble {
+	__fast: boolean;
+	__slow: boolean;
+}
+
+export class BubbleEvent<T extends EventEmitter & ILocation> {
+	private items: Array<T & IDoubleBubble> = [];
+
+	public addItem(item: T) {
+		const dItem = {...item, __fast: false, __slow: false};
+		this.items.push(dItem);
+	}
+	public removeItem(item: T) {
+		throw new Error('not implemented');
+		this.items = this.items.filter((c) => c !== item);
+	}
+	private runInitialBubble(item: T & IDoubleBubble) {
+		// TODO
+	}
+	private runFastBubble() {
+		// TODO
+	}
+	private runNormalBubble() {
+		// TODO
+	}
+	private runSlowBubble() {
+		// TODO
+	}
 }
